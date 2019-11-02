e2be12982f14a87b6298b64dc39110b8f3556752
Alec Cunningham <aleccunningham96@gmail.com>
2019-11-01 19:51:24 -0500

    Initial commit

---
+++ scripts/install_k8s_release_binaries.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -q --show-progress --https-only --timestamping \
+  "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-apiserver" \
+  "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-controller-manager" \
+  "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-scheduler" \
+  "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kubectl"
